06aa28ee52560f874f96f96dfa736fc34b261d98
Taylor Martyr <activistnetwork.ai@gmail.com>
2026-01-27 17:40:00 -0800

    home8

--- apps/web/components/landing/HomeAbout.tsx
+++ apps/web/components/landing/HomeAbout.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import { HomeAboutSection } from "../sections/HomeAboutSection";
 
 export default function HomeAbout() {
+    const title = (
+        <span>
+            <strong>MORE:</strong> Mindfulness-Oriented Recovery Enhancement
+        </span>
+    );
+
     return (
         <HomeAboutSection
             variant="default"
-            title={
-                <span>
-                    <strong>MORE:</strong> Mindfulness-Oriented Recovery Enhancement
-                </span>
-            }
+            title={title}
             subtitle="A Therapy Based on Science and Lasting Outcomes"
             description="Designed by Dr. Eric Garland, PhD, LCSW, Endowed Professor in Health Sciences at the University of California San Diego, Director of UCSD's ONEMIND and leading expert on the use of mind-body therapies. Having authored 290 scientific articles, Dr. Garland is the world's most prolific author of mindfulness research."
             profileImage={{

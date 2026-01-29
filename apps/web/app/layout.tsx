3c5f28ea8c34e302b295f82e9507cccb176d4052
Taylor Martyr <activistnetwork.ai@gmail.com>
2026-01-28 23:57:44 -0800

    track code

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
             <body>
                 <ThemeInit />
                 <SessionProvider>{children}</SessionProvider>
+                <script type="text/javascript" id="hs-script-loader" async defer src="//js-na2.hs-scripts.com/243662289.js"></script>
             </body>
         </html>
     );

b062025a858647dab80a92fecd7f33edde00133b
Taylor Martyr <activistnetwork.ai@gmail.com>
2026-02-09 22:30:34 -0800

    fhg

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -15,6 +15,15 @@ const nextConfig = {
         }
         return config;
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "tgoixnitnqhxtnvodttq.supabase.co",
+                pathname: "/storage/v1/object/public/**",
+            },
+        ],
+    },
 };
 
 export default withFlowbiteReact(nextConfig);

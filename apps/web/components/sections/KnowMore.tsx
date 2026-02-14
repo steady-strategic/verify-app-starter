fef1bdf9385f2aa036e9bc9d503d2b2a1b8b54af
Taylor Martyr <activistnetwork.ai@gmail.com>
2026-02-14 11:09:07 -0800

    ups

--- apps/web/components/sections/KnowMore.tsx
+++ apps/web/components/sections/KnowMore.tsx
@@ -29,11 +29,11 @@ export const KnowMore: React.FC<KnowMoreSectionProps> = ({
                     </p>
                 </div>
                 <Link
-                    href="/patients#patients-form"
+                    href="/know-more"
                     className="cursor-pointer border-none py-4 px-6 bg-primary-1 rounded-md flex items-center justify-center hover:bg-[#BA00B8] transition-colors"
                 >
                     <span className="text-lg font-bold leading-[150%] text-white">
-                        Sign Up
+                        {cta.text}
                     </span>
                 </Link>
             </div>
